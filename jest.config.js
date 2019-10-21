32b28868cebe2e7b4d2b0de9db127dc8661dc61d
ycjcl868 <45808948@qq.com>
2019-10-21 13:33:00 +0800

    fix: ci coverage

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverageFrom: [
+    '!**/ui.umd.js',
+    '!**/fixtures/**',
+    '!**/__snapshots__/**',
+    '!**/ui/**',
+    '!**/examples/**',
+    '!**/locales/**',
+    '!**/typings/**',
+    '!**/types/**',
+  ],
+};
