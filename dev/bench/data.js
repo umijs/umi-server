e8acca906a033a354ab5d2947d0a9df5e667e2d9
github-action-benchmark <github@users.noreply.github.com>
2019-12-04 09:36:44 +0000

    add Benchmark (benchmarkjs) benchmark result for ed9bf967debd87bd7691179cf73bdfef82de1c0d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1575451345003,
+  "lastUpdate": 1575452204211,
   "repoUrl": "https://github.com/umijs/umi-server",
   "entries": {
     "Benchmark": [
@@ -76,6 +76,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.03%",
             "unit": "ops/sec",
             "extra": "75 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45808948@qq.com",
+            "name": "信鑫-King",
+            "username": "ycjcl868"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ed9bf967debd87bd7691179cf73bdfef82de1c0d",
+          "message": "chore: benchmark (#49)\n\n* chore: benchmark\r\n\r\n* chore: benchmark\r\n\r\n* chore: benchmark\r\n\r\n* fix: github api\r\n\r\n* fix: console\r\n\r\n* chore: benchmark\r\n\r\n* fix: benchmark\r\n\r\n* fix: threshold\r\n\r\n* fix: benchmark\r\n\r\n* fix: push\r\n\r\n* fix: deploy\r\n\r\n* fix: auto push",
+          "timestamp": "2019-12-04T17:33:33+08:00",
+          "tree_id": "566aa61715dba2347d17338f051cc8504c57cfd3",
+          "url": "https://github.com/umijs/umi-server/commit/ed9bf967debd87bd7691179cf73bdfef82de1c0d"
+        },
+        "date": 1575452203898,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "render /",
+            "value": 222,
+            "range": "±118.12%",
+            "unit": "ops/sec",
+            "extra": "48 samples"
+          },
+          {
+            "name": "render /news/1",
+            "value": 282,
+            "range": "±2.87%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
           }
         ]
       }
